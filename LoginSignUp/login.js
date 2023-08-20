785b2cc89ed9745f9ca47068d4ee18f1ca59dde3
Atoland00 <tolandadam00@gmail.com>
2023-08-20 15:12:56 -0500

    done

--- LoginSignUp/login.js
+++ LoginSignUp/login.js
@@ -15,3 +15,42 @@ signupForm.addEventListener("submit", function (event) {
     const password = document.getElementById("signup-password").value;
     // Add signup logic here (e.g., AJAX request to a server)
 });
+const express = require('express');
+const bcrypt = require('bcrypt');
+const sqlite3 = require('sqlite3').verbose(); // If using SQLite
+
+const app = express();
+app.use(express.json());
+
+const db = new sqlite3.Database('your-database.db'); // Connect to your database
+
+app.post('/register', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: 'Missing required fields' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    db.run(
+      'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+      [username, email, hashedPassword],
+      (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ message: 'Failed to register user' });
+        }
+        res.json({ message: 'User registered successfully' });
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
